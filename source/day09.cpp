ff407b7f780fadd8138ecb39b48d89bdf1119a09
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-12-09 12:51:50 +0100

    minor

--- source/day09.cpp
+++ source/day09.cpp
@@ -99,6 +99,7 @@ auto day09(int argc, char** argv) -> int
     // part2
     // we simply most perform a BFS (wave propagation)
     vector<u64> basin_sizes;
+    basin_sizes.reserve(low_points.size());
     decltype(map) visited = decltype(map)::Zero(map.rows(), map.cols());
     queue<pair<i64, i64>> q;
 
